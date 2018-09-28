ee6ae83705c9fb7aaf1ac829b602b156f5157495
NachoSanchez <m.ign.sanchez@gmail.com>
2018-09-28 12:12:59 -0300

    modif

---
+++ faqs.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <title>FAQ's</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/bootstrap-css/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <?php include ('header.php'); ?>
+
+
+    <div class="container">
+
+      <br>
+      <div class="titulo">
+        <h3>PREGUNTAS <span>FRECUENTES</span></h3>
+      </div>
+
+
+      <p>
+        <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#Talles" role="button" aria-expanded="false" aria-controls="collapseExample">
+          Nuestros <span>TALLES</span>
+        </button>
+        <div class="answers collapse" id="Talles" >
+          <div >
+
+            <p>
+              Nuestra idea es que cada cliente se lleve la remera que le quede perfecta, y más allá de que nuestros talles tengan medidas comunes, te recomendamos que tomes un minuto para medir una remera que te quede bien, y la compares con las que te vamos a mostrar a continuación.
+
+              Sólo tenes que tener en cuenta que la primer medida es el ancho medido de axila a axila y la segunda es el largo!
+
+              <table class="table">
+                <h5>Remeras <span>HOMBRE</span> </h5>
+                <thead>
+                  <tr>
+                    <th scope="col">TALLE</th>
+                    <th scope="col">ANCHO</th>
+                    <th scope="col">LARGO</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <!--  TALLE XS -->
+                    <td> <span>XS</span> </td>
+                    <td>44 cm.</td>
+                    <td>66 cm.</td>
+                  </tr>
+                  <tr>
+                    <!-- TALLE S -->
+                    <td> <span>S</span> </td>
+                    <td>48 cm.</td>
+                    <td>70 cm.</td>
+                  </tr>
+                  <tr>
+                    <!-- TALLE M -->
+                    <td> <span>M</span> </td>
+                    <td>53 cm.</td>
+                    <td>72 cm.</td>
+                  </tr>
+                    <!-- TALLE L -->
+                  <tr>
+                    <td> <span>L</span> </td>
+                    <td>56 cm.</td>
+                    <td>74 cm.</td>
+                  </tr>
+                    <!-- TALLE XL -->
+                  <tr>
+                    <td> <span>XL</span> </td>
+                    <td>59 cm.</td>
+                    <td>76 cm.</td>
+                  </tr>
+                    <!-- TALLE XXL -->
+                  <tr>
+                    <td> <span>XXL</span> </td>
+                    <td>62 cm.</td>
+                    <td>80 cm.</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <br>
+
+              <table class="table">
+                <h5>Remeras <span>MUJER</span> </h5>
+                <thead>
+                  <tr>
+                    <th scope="col">TALLE</th>
+                    <th scope="col">ANCHO</th>
+                    <th scope="col">LARGO</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  <tr>
+                    <!-- TALLE S -->
+                    <td> <span>S</span> </td>
+                    <td>39 cm.</td>
+                    <td>59 cm.</td>
+                  </tr>
+                  <tr>
+                    <!-- TALLE M -->
+                    <td> <span>M</span> </td>
+                    <td>43 cm.</td>
+                    <td>61 cm.</td>
+                  </tr>
+                    <!-- TALLE L -->
+                  <tr>
+                    <td> <span>L</span> </td>
+                    <td>46 cm.</td>
+                    <td>63 cm.</td>
+                  </tr>
+                    <!-- TALLE XL -->
+                  <tr>
+                    <td> <span>XXL</span> </td>
+                    <td>48 cm.</td>
+                    <td>66 cm.</td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+              <br>
+
+              <table class="table">
+                <h5> <span>BUZOS</span> </h5>
+                <thead>
+                  <tr>
+                    <th scope="col">TALLE</th>
+                    <th scope="col">ANCHO</th>
+                    <th scope="col">LARGO</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  <tr>
+                    <!-- TALLE XS -->
+                    <td> <span>XS</span> </td>
+                    <td>49 cm.</td>
+                    <td>70 cm.</td>
+                  </tr>
+                  <tr>
+                    <!-- TALLE S -->
+                    <td> <span>S</span> </td>
+                    <td>52 cm.</td>
+                    <td>73 cm.</td>
+                  </tr>
+                    <!-- TALLE M -->
+                  <tr>
+                    <td> <span>M</span> </td>
+                    <td>54 cm.</td>
+                    <td>75 cm.</td>
+                  </tr>
+                    <!-- TALLE L -->
+                  <tr>
+                    <td> <span>L</span> </td>
+                    <td>56 cm.</td>
+                    <td>77 cm.</td>
+                  </tr>
+                    <!-- TALLE XL -->
+                  <tr>
+                    <td> <span>XL</span> </td>
+                    <td>60 cm.</td>
+                    <td>79 cm.</td>
+                  </tr>
+                    <!-- TALLE XXL -->
+                  <tr>
+                    <td> <span>XXL</span> </td>
+                    <td>64 cm.</td>
+                    <td>82 cm.</td>
+                  </tr>
+
+                </tbody>
+              </table>
+
+          </div>
+        </div>
+      </p>
+
+
+      <p>
+        <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#Calidad" role="button" aria-expanded="false" aria-controls="collapseExample">
+            <span>CALIDAD</span>
+        </button>
+        <div class="answers collapse" id="Calidad" >
+          <div>
+            <p>
+            Son remeras <span>100% ALGODÓN Peinado 24.1 Soft</span> (o sea, muy suaves). No se deforman, ni pierden el color.
+            Tienen costuras reforzadas, tapa costura y cuello Reeb.
+            Sus estampados están realizados con <span>SERIGRAFÍA TEXTIL</span>. Las estampas realizadas con este método duran muchísimo! Y son muy lindas al tacto, dado que NO es algo que se pega a la remera y que se puede salir.          </div>
+          </div>
+
+        </p>
+
+        <p>
+          <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#Envios" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Hacemos <span>ENVIOS</span>
+          </button>
+          <div class="answers collapse" id="Envios" >
+
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </div>
+        </p>
+
+        <p>
+          <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#collapseFive" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Facilidades de <span>PAGO</span>
+          </button>
+          <div class="answers collapse" id="collapseFive" >
+
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </div>
+        </p>
+
+        <p>
+          <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#collapseSix" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Donde <span>ESTAMOS</span>
+          </button>
+          <div class="answers collapse" id="collapseSix" >
+
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </div>
+        </p>
+
+        <p>
+          <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#collapseSeven" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Otra Pregunta
+          </button>
+          <div class="answers collapse" id="collapseSeven"  >
+
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+
+          </div>
+        </p>
+
+        <p>
+          <button type="button" class="btn btn-query btn-lg btn-block"data-toggle="collapse" href="#Dudas" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Tengo  <span>MUCHAS DUDAS</span>
+          </button>
+          <div class="answers collapse" id="Dudas" >
+
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+
+          </div>
+        </p>
+
+
+
+        </div>
+
+        <?php include('footer.php'); ?>
+
+
+
+
+    </div>
+
+
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
