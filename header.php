c37c844fa8d6254b1080919d98cbd90b5a5e0f0f
NachoSanchez <m.ign.sanchez@gmail.com>
2018-10-03 13:05:19 -0300

    links en header agregados

--- header.php
+++ header.php
@@ -9,24 +9,24 @@
 
             </div>
 
-    
+
     <ul class="nav-items">
-       
+
             <li><a href="index.php"> <span>HOME</span></a></li>
-            <li><a href="#"><span>FAQS</span></a></li>
+            <li><a href="faqs.php"><span>FAQ's</span></a></li>
             <li id="sub-menu"><a href="#"><span>PRODUCTOS</span></a>
                 <ul class="productos">
-                    <li><a class="" href="#">Remeras</a></li>
+                    <li><a class="" href="producto.php#">Remeras</a></li>
                     <li><a class="" href="#">Camperas</a></li>
                     <li><a class="" href="#">Pantalones</a></li>
                 </ul>
             </li>
             <li><a href="#"><span>LOGIN</span></a></li>
-            <li><a href="#"><span>REGISTER</span></a></li>
+            <li><a href="registrarUsuario.php"><span>REGISTER</span></a></li>
             <li><a class="" href="#"><img src="imagenes/carrito2.png" style="width: 30px"></a></li>
 
             <form class="form-inline my-2 my-lg-0 mr-auto ml-auto">
-                <input class="form-control mr-sm-2" type="Buscar" placeholder="Search">
+                <input class="form-control mr-sm-2" type="Buscar" placeholder="Buscar...">
                 <button class="btn btn-danger mr-auto ml-auto" type="submit">Buscar</button>
             </form>
 
