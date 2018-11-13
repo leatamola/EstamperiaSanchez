423bb6d8db046dfa43090669404737d3b14b5cba
Leandro Támola <leamacbook@LeaMacBookPro.local>
2018-11-13 12:00:21 -0300

    Rediseño de Registro y validacion Javascript

--- registrarUsuario.php
+++ registrarUsuario.php
@@ -19,6 +19,7 @@ $autenticador->registrar($_POST);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/bootstrap-css/css/bootstrap.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/header.css">
   <link rel="stylesheet" type="text/css" href="css/footer.css">
   <link rel="stylesheet" type="text/css" href="css/registro.css">
@@ -46,9 +47,8 @@ $autenticador->registrar($_POST);
           <h3> Tus mejores estampas, a un click </h2>
         </div>
       </div>
-
     </div>
-    <div class="form-content container centrar-contenido">
+    <div class="container">
       <?php
       if($validaRegisto->getErrorNombre()!=""){
         echo $validaRegisto->getErrorNombre();
@@ -77,44 +77,36 @@ $autenticador->registrar($_POST);
       // }
 
        ?>
-      <div class="row justify-content">
-        <div class="col-12 col-md-8 col-lg-6">
-          <form class="row justify-content" action="" method="post" enctype="multipart/form-data">
-
-            <label class="etiqueta col-12 col-md-10 col-lg-10" for="nombreDelUsuario">Nombre:</label>
-
-              <input class="col-12 col-md-10 col-lg-10" type="text" id="nombreDelUsuario" placeholder="Ingrese su nombre" name="userName"
-               value = <?php echo $_POST['userName']??'';?>>
-
-
-            <br><br>
-
-            <label class="etiqueta col-12  col-md-10 col-lg-10">Teléfono:</label>
-            <input class="col-12 col-md-10 col-lg-10" type="tel" placeholder="Ingrese su teléfono" name="userPhone" value=<?php echo $_POST['userPhone']??'';?>>
-
-
-            <br><br>
-
-            <label class="etiqueta col-12 col-md-10 col-lg-10">Email:</label>
-
-            <input class="col-12 col-md-10 col-lg-10" type="text" name="userMail" placeholder="example@example.com" value=<?php echo $_POST['userMail']??'';?>>
-
-            <br><br>
-
-
-            <label class="etiqueta col-12 col-md-10 col-lg-10" for="">Contraseña:</label>
-
-            <input class="col-12 col-md-10 col-lg-10" type="password" name="userPass" maxlength="20"  placeholder="********" value=<?php echo $_POST['userPass']??'';?> >
-
-            <br><br>
-
-            <label class="etiqueta col-12 col-md-10 col-lg-10" for="">Repetir contraseña:</label>
-
-            <input class="col-12 col-md-10 col-lg-10" type="password" name="userPassCheck" maxlength="20" placeholder="********" value=<?php echo $_POST['userPassCheck']??'';?> >
-
-            <br><br>
-
-            <label class="etiqueta col-12 offset-md-1 col-md-2 col-lg-2">Género</label>
+			<div class="row justify-content-center">
+				<div class="col-md-8">
+					<form method="post" class="contact-form">
+						<div class="form-group">
+							<label for="fullName">Nombre completo:</label>
+							<input type="text" name="userName" id="fullName" class="form-control" value=<?php echo $_POST['userName']??'';?>>
+							<div class="invalid-feedback"></div>
+						</div>
+						<div class="form-group">
+							<label for="email">Correo electrónico:</label>
+							<input type="email" name="userMail" id="email" class="form-control" value=<?php echo $_POST['userMail']??'';?>>
+							<div class="invalid-feedback"></div>
+						</div>
+						<div class="form-group">
+							<label for="phone">Teléfono de contacto:</label>
+							<input type="text" name="userPhone" id="phone" class="form-control" value=<?php echo $_POST['userPhone']??'';?>>
+							<div class="invalid-feedback"></div>
+						</div>
+						<div class="form-group">
+							<label for="password">Contraseña:</label>
+							<input type="password" name="userPass" id="password" class="form-control" value=<?php echo $_POST['userPass']??'';?>>
+							<div class="invalid-feedback"></div>
+						</div>
+						<div class="form-group">
+							<label for="rePassword">Repetir Contraseña:</label>
+							<input type="password" name="userPassCheck" id="rePassword" class="form-control" value=<?php echo $_POST['userPassCheck']??'';?>>
+							<div class="invalid-feedback"></div>
+						</div>
+						<div class="form-group">
+            <label class="etiqueta col-12 offset-md-1 col-md-2 col-lg-2">Género:</label>
               <label class="col-6 col-md-4"><input type="radio" name="userGender" value="m"
                 <?php
                 if(!isset($_POST['userGender'])){
@@ -127,9 +119,8 @@ $autenticador->registrar($_POST);
                   echo '';
                 }else if ($_POST['userGender']==='f') {
                     echo 'checked';}?> > Femenino</label><label class="none col-0 col-md-1"></label>
+              </div>
 
-
-              <br><br>
               <label for="">
                 Avatar:
                 <input type="file" name="foto" value="">
@@ -140,14 +131,17 @@ $autenticador->registrar($_POST);
             <div class="col-12 centrar-contenido">
               <button class="nice-bottom"type="submit">Registrarse</button>
             </div>
-          </form>
-        </div>
+					</form>
+				</div>
+			</div>
+		</div>
 
-      </div>
-    </div>
+
+
 
   </section>
   	<?php include ('footer.php'); ?>
+<script src="js/app.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
